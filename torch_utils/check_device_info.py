d72cf5df6cb40bc1182e3be834fb345ae9c85b61
Mathias <mathiasbensimhon@gmail.com>
2023-01-29 22:07:19 +0200

    torch and cuda utils added

---
+++ torch_utils/check_device_info.py
@@ -0,0 +1,8 @@
+!pip install pycuda
+import torch
+import pycuda.driver as cuda
+cuda.init()
+## Get Id of default device
+torch.cuda.current_device()
+# 0
+cuda.Device(0).name() # '0' is the id of your GPU
