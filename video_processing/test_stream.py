7cfe2bf3eca98b7748e52ef04f9c0f5bc571d45e
Mathias <mathiasbensimhon@gmail.com>
2023-02-19 13:23:27 +0200

    update video processing

--- video_processing/test_stream.py
+++ video_processing/test_stream.py
@@ -1,28 +1,29 @@
 import cv2
 import time
+import sys
 
 def main():
-    window_title = "CSI Camera"
+    print("capture screenshot using y key")
+    if len(sys.argv) < 2:
+        print("usage: python3 test_stream.py <device number> ")
+    video_capture = cv2.VideoCapture(f"/dev/video{sys.argv[1]}")
+    #define resolution
+    video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
     
-    # video_capture = cv2.VideoCapture("/dev/video0")
-    video_capture = cv2.VideoCapture("rtsp://10.0.0.12:8554/test")
-    video_capture.set(cv2.CAP_PROP_BUFFERSIZE,1)
+    
+    # video_capture = cv2.VideoCapture("rtsp://10.0.0.12:8554/test")
+    # video_capture.set(cv2.CAP_PROP_BUFFERSIZE,1)
 
     start = time.time()
     paused = False
     print(f"FPS: {int(video_capture.get(cv2.CAP_PROP_FPS))}")
+    i  =  0
     if video_capture.isOpened():
         try:
             
             # window_handle = cv2.namedWindow(window_title, cv2.WINDOW_AUTOSIZE)
             while True:
-                if (time.time() - start > 5) and paused == False:
-                    print("inside")
-                    time.sleep(5)
-                    video_capture.release()
-                    video_capture = cv2.VideoCapture("rtsp://10.0.0.12:8554/test")
-                    video_capture.set(cv2.CAP_PROP_BUFFERSIZE,1)
-                    paused = True
                 t0 = time.time()
                 ret, img = video_capture.read()
                 # print(f"time for capture {time.time() - t0}")
@@ -33,6 +34,13 @@ def main():
                 # Stop the program on the ESC key or 'q'
                 if keyCode == 27 or keyCode == ord('q'):
                    break
+               # take a screenshot
+                if keyCode == 27 or keyCode == ord('y'):
+                    path = f'test_{i}.jpg'
+                    cv2.imwrite(path,img)
+                    print(f'image captured and path is {path}')
+                    i += 1
+                    
         finally:
             video_capture.release()
             cv2.destroyAllWindows()
